53f1808b5d5c27af43300fb2c4ba29db7a061171
Liz House <lizehouse@gmail.com>
2018-02-20 20:48:32 -0700

    fialed attempt to fix index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Climbing' });
-});
-
+  res.render('index', { title: "Climbing" })
+})
+// tried send, and variations of render. still cant get it.
 module.exports = router;
